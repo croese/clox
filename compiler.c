6d67fd626e044a7a56e7201e8441990640a6b258
Christian Roese <christian.roese@mortonbuildings.com>
2020-03-06 10:24:06 -0600

    adds rest of basic compiler code and start of bytecode emission code

--- compiler.c
+++ compiler.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "common.h"
 #include "compiler.h"
@@ -8,11 +9,20 @@ typedef struct {
   Token current;
   Token previous;
   bool hadError;
+  bool panicMode;
 } Parser;
 
 Parser parser;
 
+Chunk* compilingChunk;
+
+static Chunk* currentChunk() { return compilingChunk; }
+
 static void errorAt(Token* token, const char* message) {
+  if (parser.panicMode)
+    return;
+  parser.panicMode = true;
+
   fprintf(stderr, "[line %d] Error", token->line);
 
   if (token->type == TOKEN_EOF) {
@@ -45,10 +55,40 @@ static void advance() {
   }
 }
 
-bool compile(const char* source) {
+static void consume(TokenType type, const char* message) {
+  if (parser.current.type == type) {
+    advance();
+    return;
+  }
+
+  errorAtCurrent(message);
+}
+
+static void emitByte(uint8_t byte) {
+  writeChunk(currentChunk(), byte, parser.previous.line);
+}
+
+static void emitBytes(uint8_t byte1, uint8_t byte2) {
+  emitByte(byte1);
+  emitByte(byte2);
+}
+
+static void emitReturn() { emitByte(OP_RETURN); }
+
+static void endCompiler() { emitReturn(); }
+
+static void expression() {}
+
+bool compile(const char* source, Chunk* chunk) {
   initScanner(source);
+  compilingChunk = chunk;
+
+  parser.hadError = false;
+  parser.panicMode = false;
+
   advance();
   expression();
   consume(TOKEN_EOF, "Expect end of expression.");
+  endCompiler();
   return !parser.hadError;
 }
